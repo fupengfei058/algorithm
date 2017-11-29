1aa5c39826c1a25480658fdca31a88ef23e0c609
fupengfei <fupengfei@yaochufa.com>
2017-11-29 17:15:58 +0800

    11/29 UPDATE

--- others/33.cpp
+++ others/33.cpp
@@ -4,6 +4,42 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-void LCS(const char* str1,const char* str2,string& str){
-	
+int max(int a, int b)
+{
+    return (a>b)? a:b;
+}  
+/**
+ * 返回X[0...m-1]和Y[0...n-1]的LCS的长度
+ */
+int lcs(string &X, string &Y, int m, int n)
+{
+    // 动态规划表，大小(m+1)*(n+1)
+    vector<vector<int>> table(m+1,vector<int>(n+1));
+    for(int i=0; i<m+1; ++i)
+    {
+        for(int j=0; j<n+1; ++j)
+        {
+            // 第一行和第一列置0
+            if (i == 0 || j == 0)
+                table[i][j] = 0;
+
+            else if(X[i-1] == Y[j-1])
+                table[i][j] = table[i-1][j-1] + 1;
+
+            else
+                table[i][j] = max(table[i-1][j], table[i][j-1]);
+        }
+    }
+    return table[m][n];
 }
+int main()
+{
+    string X = "ABCBDAB";
+    string Y = "BDCABA";
+    cout << "The length of LCS is " << lcs(X, Y, X.length(), Y.length());
+    cout << endl;
+    getchar();
+    return 0;
+}
+// 求最长公共子字符串和最长子序列的区别在于：
+//最长子字符串当str1[i] ！= str2[j]时，veca[i][j]长度要为0，而不是max{veca[i - 1][j], veca[i][j - 1]}
